655bf63675ca3c890c29bd55aa300f388fa05986
BWMarket LLC <zahir@bankway.group>
2024-12-18 09:59:29 -0600

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,10 +2,10 @@ export const contactsData = {
     email: 'DevZahirLLC@gmail.com',
     phone: 'Not Initialized',
     address: 'Not Initialized ',
-    github: 'https://github.com/devzahir',
+    github: 'https://github.com/devzahirx3',
     facebook: 'https://www.facebook.com/devzahir/',
     linkedIn: 'https://www.linkedin.com/in/devzahir/',
     twitter: 'https://twitter.com/devzahir',
     stackOverflow: '',
-    devUsername: "devzahir"
+    devUsername: "devzahirx3"
 }
