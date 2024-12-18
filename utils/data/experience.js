cf0f9f4ca2dd8070d0e14f06b80d7507d0ca16ab
BWMarket LLC <zahir@bankway.group>
2024-12-18 12:31:15 -0600

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,6 +3,6 @@ export const experiences = [
     id: 1,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    duration: "(Jun 2021 - Present)"
   }
 ]
