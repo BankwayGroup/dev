34cb389d5a40d917ec3d1c9287802bf592a7fdba
BWMarket LLC <zahir@bankway.group>
2024-12-17 18:51:59 -0600

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profile.png',
   designation: "Programmer Developer",
   description: "Hey, I’m Dev Zahir—a tech enthusiast and programmer who loves turning ideas into reality. Whether I’m coding Telegram bots, building vibrant Discord servers, or crafting web applications, I’m all about creating cool, functional digital experiences. JavaScript is my go-to, but I’ve got skills across the board and a knack for picking up new tech fast. Problem-solving is my jam, and I’m always exploring fresh tools and trends to level up. If you need someone who’s innovative, adaptable, and ready to bring energy to the table, let’s team up!",
-  email: 'devzahirllc@gmail.com',
+  email: 'DevZahirLLC@gmail.com',
   phone: 'Not Initialized',
   address: 'Not Initialized',
   github: 'https://github.com/devzahir',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: '',
   leetcode: "https://leetcode.com/devzahir/",
   devUsername: "devzahir",
-  resume: ""
+  resume: "https://t.me/devzahirbot?start="
 }
