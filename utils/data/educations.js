a6323b8159f7c95bdc3d7e84894ed75832e71c91
BWMarket LLC <zahir@bankway.group>
2024-12-18 12:19:19 -0600

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,7 +2,7 @@ export const educations = [
   {
     id: 1,
     title: "Python | Flaskapp | Django | HTML | Javascript | Fastapi",
-    duration: "2020 - Present",
+    duration: "Special Skills",
     institution: "",
   }
 ]
