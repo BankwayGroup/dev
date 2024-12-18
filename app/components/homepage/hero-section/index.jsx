d6187d38473a1d45bf959838363c029059015aa7
BWMarket LLC <zahir@bankway.group>
2024-12-18 14:33:30 -0600

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -9,6 +9,8 @@ import { FaRobot } from 'react-icons/fa';
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 import { FaSuitcase } from 'react-icons/fa';  // Fiverr icon import
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 
 function HeroSection() {
   return (
@@ -21,17 +23,24 @@ function HeroSection() {
         className="absolute -top-[98px] -z-10"
       />
 
-      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            Hello, <br />
-            This is {' '}
-            <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a Professional `}
-            <span className=" text-[#16f2b3]">{personalData.designation}</span>
-            .
-          </h1>
-
+        <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+          Hello, <br />
+          This is{' '}
+          <span className="text-pink-500">{personalData.name}</span>
+          {` , I'm a Professional `}
+          <span className="text-[#16f2b3]">
+            <Typewriter
+              words={[personalData.designation, 'Developer', 'Designer']}
+              loop={true}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+          .
+        </h1>
           <div className="my-12 flex items-center gap-5">
 
      <Link
