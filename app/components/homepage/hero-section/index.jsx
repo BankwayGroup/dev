b4b10e19d1d5759eecc0de482261e0172f8f8807
BWMarket LLC <zahir@bankway.group>
2024-12-18 16:36:33 -0600

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -7,7 +7,6 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaFacebook, FaTwitterSquare, FaRobot, FaSuitcase } from "react-icons/fa"; // Combine imports
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
-import Typewriter from 'typewriter-effect';
 
 function HeroSection() {
   return (
@@ -23,25 +22,14 @@ function HeroSection() {
 <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
   <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
     <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-      <span className="text-white">
-        <Typewriter
-          words={[
-            "Hello,",
-            `This is ${personalData.name}`,
-            `, I'm a Professional`,
-            personalData.designation,
-          ]}
-          loop={false} // You can change this to `true` if you want the animation to repeat
-          cursor
-          cursorStyle="|"
-          typeSpeed={70} // Adjust typing speed here
-          deleteSpeed={50} // Adjust delete speed
-          delaySpeed={1000} // Delay before retyping
-        />
+      <span className="text-white typewriter-effect">
+        Hello, <br />
+        This is{' '}
+        <span className="text-pink-500">{personalData.name}</span>
+        {`, I'm a Professional`} <br />
+        <span className="text-[#16f2b3]">{personalData.designation}</span>
       </span>
     </h1>
-
-
           <div className="my-12 flex items-center gap-5">
             <Link
               href="https://www.fiverr.com/devzahir"
