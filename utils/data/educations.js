2540125218b5080c310a50f1050d7a70831b96f7
BWMarket LLC <zahir@bankway.group>
2024-12-17 20:20:54 -0600

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,7 +1,7 @@
 export const educations = [
   {
     id: 1,
-    title: "Python | Flaskapp | Django",
+    title: "Python | Flaskapp | Django | HTML",
     duration: "2020 - Present",
     institution: "",
   }
