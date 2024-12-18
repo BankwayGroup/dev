d1982bfa5d6d49cf6b6086ca5d0db72181ca7fa4
BWMarket LLC <zahir@bankway.group>
2024-12-17 20:10:11 -0600

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,6 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
-  },
-  {
-    id: 2,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
     duration: "(Jun 2021 - Jan 2022)"
