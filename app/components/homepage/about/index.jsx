3dd611d4bb4600c89c21b725a743f3f4d1334cfc
BWMarket LLC <zahir@bankway.group>
2024-12-20 22:10:43 -0600

    Update index.jsx

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -105,6 +105,7 @@ function AboutSection() {
         <h2 className="text-2xl font-bold text-[#16f2b3] uppercase mb-8 flex items-center">
           <i className="fas fa-box-open mr-3"></i> Packages
         </h2>
+
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {plans.map((plan) => (
             <div
