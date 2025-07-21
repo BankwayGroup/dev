48e4d96f8a4d14e6f01ef315d298a118bb082dc9
BWMarket LLC <zahir@bankway.group>
2025-07-21 03:16:45 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -4,10 +4,9 @@ import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
-import { MdDownload } from "react-icons/md";
+import { FaFacebook, FaTwitterSquare, FaRobot, FaSuitcase } from "react-icons/fa"; 
 import { RiContactsFill } from "react-icons/ri";
-import { SiLeetcode } from "react-icons/si";
+import { SiWhatsapp } from "react-icons/si";
 
 function HeroSection() {
   return (
