7b771d16945dc1e811edd72ad0687d1304a066b1
BWMarket LLC <zahir@bankway.group>
2025-07-21 03:03:14 -0500

    Update index.jsx

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -17,14 +17,7 @@ function ContactSection() {
         <ContactForm />
         <div className="lg:w-3/4 ">
           <div className="flex flex-col gap-5 lg:gap-9">
-            <p className="text-sm md:text-xl flex items-center gap-3">
-              <MdAlternateEmail
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>{personalData.email}</span>
-            </p>
-      
+       
         <p className="text-sm md:text-xl flex items-center gap-3">
         <FaGlobe
           className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
