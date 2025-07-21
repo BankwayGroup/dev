33592bc308f16086d52884a9fcd00830026cef92
BWMarket LLC <zahir@bankway.group>
2025-07-21 01:59:06 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -85,62 +85,57 @@ function HeroSection() {
           </div>
 
         </div>
-        <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
-          <div className="flex flex-row">
-            <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
-            <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
-          </div>
-          <div className="px-4 lg:px-8 py-5">
-            <div className="flex flex-row space-x-2">
-              <div className="h-3 w-3 rounded-full bg-red-400"></div>
-              <div className="h-3 w-3 rounded-full bg-orange-400"></div>
-              <div className="h-3 w-3 rounded-full bg-green-200"></div>
-            </div>
-          </div>
-          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
-            <code className="font-mono text-xs md:text-sm lg:text-base">
-              <div className="blink">
-                <span className="mr-2 text-pink-500">const</span>
-                <span className="mr-2 text-white">coder</span>
-                <span className="mr-2 text-pink-500">=</span>
-                <span className="text-gray-400">{'{'}</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-                <span className="text-gray-400">{`'`}</span>
-                <span className="text-amber-300">Dev Zahir</span>
-                <span className="text-gray-400">{`',`}</span>
-              </div>
-              <div className="ml-4 lg:ml-8 mr-2">
-                 <span className="text-white">skills:</span>
-  <span className="text-gray-400">{['}</span>
-  <span className="text-amber-300">HTML</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">CSS</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">Javascript</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">React</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">MongoDB</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">MySQL</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">PostgreSQL</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">Git</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">Python</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">Wordpress</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">Markdown</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">Django</span>
-  <span className="text-gray-400">{"', '"}</span>
-  <span className="text-amber-300">PHP</span>
-  <span className="text-gray-400">{"'],"}</span>
-              </div>
+    <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
+  <div className="flex flex-row">
+    <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
+    <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
+  </div>
+  <div className="px-4 lg:px-8 py-5">
+    <div className="flex flex-row space-x-2">
+      <div className="h-3 w-3 rounded-full bg-red-400"></div>
+      <div className="h-3 w-3 rounded-full bg-orange-400"></div>
+      <div className="h-3 w-3 rounded-full bg-green-200"></div>
+    </div>
+  </div>
+  <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+    <code className="font-mono text-xs md:text-sm lg:text-base block">
+      <div className="blink">
+        <span className="mr-2 text-pink-500">const</span>
+        <span className="mr-2 text-white">coder</span>
+        <span className="mr-2 text-pink-500">=</span>
+        <span className="text-gray-400">{'{'}</span>
+      </div>
+      <div>
+        <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+        <span className="text-gray-400">'</span>
+        <span className="text-amber-300">Dev Zahir</span>
+        <span className="text-gray-400">',</span>
+      </div>
+      <div className="ml-4 lg:ml-8 mr-2">
+        <span className="text-white">skills:</span>
+        <span className="text-gray-400">[</span>
+        <span className="text-amber-300">'HTML'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'CSS'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'JavaScript'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'React'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'MongoDB'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'MySQL'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'PostgreSQL'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'Git'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'Python'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'WordPress'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'Markdown'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'Django'</span><span className="text-gray-400">, </span>
+        <span className="text-amber-300">'PHP'</span>
+        <span className="text-gray-400">],</span>
+      </div>
+      <div>
+        <span className="text-gray-400">{'}'}</span>
+      </div>
+    </code>
+  </div>
+</div>
+
               <div>
                 <span className="ml-4 lg:ml-8 mr-2 text-white">hardWorker:</span>
                 <span className="text-orange-400">true</span>
