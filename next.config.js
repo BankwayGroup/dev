f5af95c506b7040917404d048935669aeb5c396e
BWMarket LLC <zahir@bankway.group>
2025-07-29 16:04:59 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    sourceMap: true,  // <-- Enable sass source maps here
   },
   images: {
     remotePatterns: [
@@ -23,4 +24,26 @@ module.exports = {
       },
     ],
   },
-}
+
+  webpack(config, options) {
+    config.module.rules.forEach((rule) => {
+      if (rule.test && rule.test.toString().includes('scss')) {
+        rule.use.forEach((loader) => {
+          if (loader.loader && loader.loader.includes('resolve-url-loader')) {
+            loader.options = {
+              ...loader.options,
+              sourceMap: true,  // <-- Enable source maps for resolve-url-loader
+            };
+          }
+          if (loader.loader && loader.loader.includes('sass-loader')) {
+            loader.options = {
+              ...loader.options,
+              sourceMap: true,  // <-- Enable source maps for sass-loader (required)
+            };
+          }
+        });
+      }
+    });
+    return config;
+  },
+};
