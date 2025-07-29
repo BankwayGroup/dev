570a40f525bdd72bf2e8edee69d406acb9d47195
BWMarket LLC <zahir@bankway.group>
2025-07-29 16:51:20 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
+const repoName = 'your-repo-name'; // 🔁 change this
+
 module.exports = {
+  output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}`,
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-    sourceMap: true, // Add source map here for sass-loader
+    sourceMap: true,
   },
+
   images: {
     remotePatterns: [
       {
@@ -26,7 +33,6 @@ module.exports = {
   },
 
   webpack(config, { dev, isServer }) {
-    // Add sourceMap:true for resolve-url-loader in the webpack config
     config.module.rules.forEach((rule) => {
       if (rule.test && rule.test.toString().includes('scss')) {
         rule.use.forEach((loader) => {
@@ -39,7 +45,6 @@ module.exports = {
         });
       }
     });
-
     return config;
   },
 };
