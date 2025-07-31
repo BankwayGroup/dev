bc8b439ab00d5025d79d69595f787a8064fc0414
BWMarket LLC <zahir@bankway.group>
2025-07-30 22:42:20 -0500

    Update index.jsx

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -10,26 +10,16 @@ import { MdAlternateEmail } from "react-icons/md";
 import ContactForm from './contact-form';
 import { SiTelegram } from 'react-icons/si'; // Add this import if not already present
 
-function ContactSection() {
-  return (
-    <div id="contact" className="my-12 lg:my-16 relative mt-24 text-white">
-      <div className="hidden lg:flex flex-col items-center absolute top-24 -right-8">
-        <span className="bg-[#1a1443] w-fit text-white rotate-90 p-2 px-5 text-xl rounded-md">
-          CONTACT
-        </span>
-        <span className="h-36 w-[2px] bg-[#1a1443]"></span>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
-        <ContactForm />
-        <div className="lg:w-3/4 ">
-          <div className="flex flex-col gap-5 lg:gap-9">
-            <p className="text-sm md:text-xl flex items-center gap-3">
-              <MdAlternateEmail
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>{personalData.email}</span>
-            </p>
+<p className="text-sm md:text-xl flex items-center gap-3">
+  <a href={`mailto:${personalData.email}`} target="_blank" rel="noopener noreferrer">
+    <MdAlternateEmail
+      className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+      size={36}
+    />
+  </a>
+  <span>{personalData.email}</span>
+</p>
+
 
           </div>
           <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">
