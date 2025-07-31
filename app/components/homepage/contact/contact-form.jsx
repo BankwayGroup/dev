07f4d2837d3e6e78bce440e8964cb21651544861
BWMarket LLC <zahir@bankway.group>
2025-07-30 23:56:21 -0500

    Update contact-form.jsx

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -17,115 +17,127 @@ function ContactForm() {
 
   const checkRequired = () => {
     if (userInput.email && userInput.message && userInput.name) {
-      setError({ ...error, required: false });
+      setError((prev) => ({ ...prev, required: false }));
     }
   };
 
-  const handleSendMail = async (e) => {
+  const handleSendMessage = async (e) => {
     e.preventDefault();
 
     if (!userInput.email || !userInput.message || !userInput.name) {
-      setError({ ...error, required: true });
+      setError((prev) => ({ ...prev, required: true }));
       return;
-    } else if (error.email) {
+    }
+
+    if (!isValidEmail(userInput.email)) {
+      setError((prev) => ({ ...prev, email: true }));
       return;
-    } else {
-      setError({ ...error, required: false });
-    };
+    }
 
     try {
       setIsLoading(true);
-      const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
-        userInput
-      );
+
+      // Telegram config
+const telegramBotToken = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
+const telegramChatId = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID;
+const topicId = Number(process.env.NEXT_PUBLIC_TELEGRAM_TOPIC_ID);
+
+
+      const telegramMessage = `🌐 DevZahir.com\nContact Form Submission:\n\nName: ${userInput.name}\nEmail: ${userInput.email}\nMessage: ${userInput.message}`;
+
+      await axios.post(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+        chat_id: telegramChatId,
+        message_thread_id: topicId,
+        text: telegramMessage,
+      });
 
       toast.success("Message sent successfully!");
-      setUserInput({
-        name: "",
-        email: "",
-        message: "",
-      });
+      setUserInput({ name: "", email: "", message: "" });
     } catch (error) {
-      toast.error(error?.response?.data?.message);
+      toast.error(error?.response?.data?.description || "Failed to send the message.");
     } finally {
       setIsLoading(false);
-    };
+    }
   };
 
   return (
     <div>
-      <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">Contact with me</p>
+      <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">Contact Zahir</p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
-        <p className="text-sm text-[#d3d8e8]">{"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}</p>
-        <div className="mt-6 flex flex-col gap-4">
+        <p className="text-sm text-[#d3d8e8]">
+          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+        </p>
+        <form onSubmit={handleSendMessage} className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
-            <label className="text-base">Your Name: </label>
+            <label className="text-base">Your Name:</label>
             <input
-              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
+              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] transition-all duration-300 px-3 py-2"
               type="text"
               maxLength="100"
-              required={true}
+              required
+              value={userInput.name}
               onChange={(e) => setUserInput({ ...userInput, name: e.target.value })}
               onBlur={checkRequired}
-              value={userInput.name}
             />
           </div>
 
           <div className="flex flex-col gap-2">
-            <label className="text-base">Your Email: </label>
+            <label className="text-base">Your Email:</label>
             <input
-              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
+              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] transition-all duration-300 px-3 py-2"
               type="email"
               maxLength="100"
-              required={true}
+              required
               value={userInput.email}
-              onChange={(e) => setUserInput({ ...userInput, email: e.target.value })}
+              onChange={(e) => {
+                setUserInput({ ...userInput, email: e.target.value });
+                setError((prev) => ({ ...prev, email: false }));
+              }}
               onBlur={() => {
                 checkRequired();
-                setError({ ...error, email: !isValidEmail(userInput.email) });
+                setError((prev) => ({ ...prev, email: !isValidEmail(userInput.email) }));
               }}
             />
             {error.email && <p className="text-sm text-red-400">Please provide a valid email!</p>}
           </div>
 
           <div className="flex flex-col gap-2">
-            <label className="text-base">Your Message: </label>
+            <label className="text-base">Your Message:</label>
             <textarea
-              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
+              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] transition-all duration-300 px-3 py-2"
               maxLength="500"
-              name="message"
-              required={true}
-              onChange={(e) => setUserInput({ ...userInput, message: e.target.value })}
-              onBlur={checkRequired}
+              required
               rows="4"
               value={userInput.message}
+              onChange={(e) => setUserInput({ ...userInput, message: e.target.value })}
+              onBlur={checkRequired}
             />
           </div>
-          <div className="flex flex-col items-center gap-3">
-            {error.required && <p className="text-sm text-red-400">
-              All fiels are required!
-            </p>}
+
+          {error.required && (
+            <p className="text-sm text-red-400 text-center">All fields are required!</p>
+          )}
+
+          <div className="flex justify-center">
             <button
-              className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-              role="button"
-              onClick={handleSendMail}
+              type="submit"
+              className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-xs md:text-sm font-medium uppercase tracking-wider text-white transition-all duration-200"
               disabled={isLoading}
             >
-              {
-                isLoading ?
-                <span>Sending Message...</span>:
-                <span className="flex items-center gap-1">
+              {isLoading ? (
+                "Sending Message..."
+              ) : (
+                <>
                   Send Message
                   <TbMailForward size={20} />
-                </span>
-              }
+                </>
+              )}
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
-};
+}
 
 export default ContactForm;
