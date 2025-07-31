8d9000451bf9f96505b7657e9cde000000aed14f
BWMarket LLC <zahir@bankway.group>
2025-07-31 15:56:05 -0500

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -47,7 +47,7 @@ function Navbar() {
           <li>
             <Link href="/blog">
               <a className="block px-4 py-2 no-underline outline-none hover:no-underline text-sm text-white transition-colors duration-300 hover:text-pink-600">
-                BLOG
+                NEWS
               </a>
             </Link>
           </li>
