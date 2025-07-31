5da387df6768d37e1288327b4375097e6f447174
BWMarket LLC <zahir@bankway.group>
2025-07-30 22:39:06 -0500

    Update index.jsx

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -39,14 +39,7 @@ function ContactSection() {
                 size={48}
               />
             </Link>
-
-    <Link target="_blank" href="https://x.com/devzahirjs">
-              <FaXTwitter
-                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={48}
-              />
-            </Link>
-    <Link target="_blank" href="https://t.me/devzahirbot?start=start">
+                <Link target="_blank" href="https://t.me/devzahirbot?start=start">
   <SiTelegram
     className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
     size={48}
@@ -58,6 +51,14 @@ function ContactSection() {
                 size={48}
               />
             </Link>
+    <Link target="_blank" href="https://x.com/devzahirjs">
+              <FaXTwitter
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+
+
           </div>
         </div>
       </div>
