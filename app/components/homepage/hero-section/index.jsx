acebdb0f6447fcb9bb9e4d7f54493eb155598868
BWMarket LLC <zahir@bankway.group>
2025-07-31 14:17:40 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -21,7 +21,11 @@ function HeroSection() {
         height={795}
         className="absolute -top-[98px] -z-10"
       />
-
+  <div className="fixed top-4 right-4 z-50 md:hidden">
+    <Link href="/#packages" aria-label="Go to Packages">
+      <FaShoppingCart size={28} className="text-white hover:text-pink-500 transition duration-300" />
+    </Link>
+  </div>
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
@@ -73,11 +77,6 @@ function HeroSection() {
 
 
 
-</div>
-<div className="absolute top-4 right-4 block md:hidden z-50">
-  <Link href="/#packages" aria-label="Go to Packages">
-    <FaShoppingCart size={28} className="text-white hover:text-pink-500 transition duration-300" />
-  </Link>
 </div>
 
           {/* Contact Button */}
