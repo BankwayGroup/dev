6202f69814db58db3c774be307e4f812fb828294
BWMarket LLC <zahir@bankway.group>
2025-07-30 19:06:05 -0500

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -20,6 +20,10 @@ function Navbar() {
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
+                   <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#packages">
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">Purchase</div>
+            </Link>
+          </li>
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
           </li>
