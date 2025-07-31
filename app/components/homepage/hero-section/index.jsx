758777ee8c14aa5d180b5f5781a3b509809fdf4a
BWMarket LLC <zahir@bankway.group>
2025-07-31 14:18:52 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -14,6 +14,12 @@ import { SiTelegram } from 'react-icons/si';
 function HeroSection() {
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
+       <div className="fixed top-4 right-4 z-50 md:hidden">
+    <Link href="/#packages" aria-label="Go to Packages">
+      <FaShoppingCart
+        size={28}
+        className="text-white hover:text-pink-500 transition duration-300"
+      />
       <Image
         src="/hero.svg"
         alt="Hero"
@@ -21,11 +27,7 @@ function HeroSection() {
         height={795}
         className="absolute -top-[98px] -z-10"
       />
-  <div className="fixed top-4 right-4 z-50 md:hidden">
-    <Link href="/#packages" aria-label="Go to Packages">
-      <FaShoppingCart size={28} className="text-white hover:text-pink-500 transition duration-300" />
-    </Link>
-  </div>
+
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
