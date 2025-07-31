7e722eb090bde73fea2dd8ab06bfa7d2945b3a2c
BWMarket LLC <zahir@bankway.group>
2025-07-30 19:12:06 -0500

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,15 +1,12 @@
 // @flow strict
 import Link from "next/link";
 
-
 function Navbar() {
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
+          <Link href="/" className="text-[#16f2b3] text-3xl font-bold">
             DEV ZAHIR
           </Link>
         </div>
@@ -20,27 +17,40 @@ function Navbar() {
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
-                   <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#packages">
+
+          <li>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#packages">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">Purchase</div>
             </Link>
           </li>
+
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience">
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div>
+            </Link>
           </li>
+
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills">
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div>
+            </Link>
           </li>
+
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERTISE</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education">
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERTISE</div>
+            </Link>
           </li>
-    
+
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects">
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div>
+            </Link>
           </li>
         </ul>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
