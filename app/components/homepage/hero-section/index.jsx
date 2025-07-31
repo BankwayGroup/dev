7eadb9669cf71e05a9bdaf8cac7b036c4d824ecc
BWMarket LLC <zahir@bankway.group>
2025-07-31 17:30:11 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -16,12 +16,11 @@ function HeroSection() {
 <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
 <a
   href="/#packages"
-  className="block md:hidden fixed top-5 right-5 z-50 w-10 h-10 flex items-center justify-center rounded-full bg-[#16f2b3] hover:bg-pink-600 transition-transform duration-300 hover:scale-110"
+  className="block md:hidden absolute top-5 right-5 z-50 w-9 h-9 flex items-center justify-center rounded-full bg-[#16f2b3] hover:bg-pink-600 transition-transform duration-300 hover:scale-110"
   aria-label="Packages"
 >
-  <FaShoppingCart size={20} className="text-black" />
+  <FaShoppingCart size={18} className="text-black" />
 </a>
-
 
 
 
