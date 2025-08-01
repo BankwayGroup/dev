bc5f1a58556dcaf443e9f08b90cb1f2a2daaa6c4
BWMarket LLC <zahir@bankway.group>
2025-07-31 21:11:53 -0500

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,12 +2,13 @@
 
 import { personalData } from "@/utils/data/personal-data";
 import BlogCard from "../components/homepage/blog/blog-card";
+import Link from "next/link";
 
 async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
@@ -18,9 +19,9 @@ async function page() {
   const blogs = await getBlogs();
 
   return (
-    <div className="py-8">
+    <div className="py-8 flex flex-col min-h-screen">
       <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
+        <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
             NEWS
@@ -29,13 +30,22 @@ async function page() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10 flex-grow">
         {
           blogs.map((blog, i) => (
             blog?.cover_image &&
             <BlogCard blog={blog} key={i} />
           ))
         }
+      </div>
+
+      {/* Back button container */}
+      <div className="flex justify-center mt-10 mb-4">
+        <Link href="/">
+          <button className="bg-[#1a1443] text-white px-6 py-2 rounded-md hover:bg-[#332f6e] transition">
+            Back to Home
+          </button>
+        </Link>
       </div>
     </div>
   );
