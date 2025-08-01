59408a69c2da702e2845e10d504d5f8b192a0dfa
BWMarket LLC <zahir@bankway.group>
2025-07-31 21:38:29 -0500

    Update index.jsx

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -11,11 +11,12 @@ import { IoLogoGithub, IoMdCall } from 'react-icons/io';
 import { MdAlternateEmail } from 'react-icons/md';
 import { SiTelegram } from 'react-icons/si';
 import ContactForm from './contact-form';
-import Image from 'next/image'; // Make sure this is at the top
+import Image from 'next/image';
 
 function ContactSection() {
   return (
     <div id="contact" className="my-12 lg:my-16 relative mt-24 text-white">
+      {/* Vertical CONTACT text on large screens */}
       <div className="hidden lg:flex flex-col items-center absolute top-24 -right-8">
         <span className="bg-[#1a1443] w-fit text-white rotate-90 p-2 px-5 text-xl rounded-md">
           CONTACT
@@ -23,35 +24,52 @@ function ContactSection() {
         <span className="h-36 w-[2px] bg-[#1a1443]"></span>
       </div>
 
+      {/* Grid layout */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
         <ContactForm />
 
-        {/* Social Icons */}
-        <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">
-          <Link target="_blank" href="https://github.com/devzahirx3/DevZahir">
-            <IoLogoGithub
-              className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-              size={48}
+        {/* Right column: Social + Buy Me a Coffee */}
+        <div className="flex flex-col items-center lg:items-start text-center lg:text-left">
+          {/* Social Icons */}
+          <div className="mt-8 lg:mt-16 flex flex-wrap justify-center lg:justify-start gap-5 lg:gap-10">
+            <Link target="_blank" href="https://github.com/devzahirx3/DevZahir">
+              <IoLogoGithub className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer" size={48} />
+            </Link>
+            <Link target="_blank" href="https://t.me/devzahirbot?start=start">
+              <SiTelegram className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer" size={48} />
+            </Link>
+            <Link target="_blank" href="https://www.facebook.com/share/1gjjhoY2Vn/?mibextid=wwXIfr">
+              <FaFacebook className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer" size={48} />
+            </Link>
+            <Link target="_blank" href="https://x.com/devzahirjs">
+              <FaXTwitter className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer" size={48} />
+            </Link>
+          </div>
+
+          {/* Buy Me a Coffee Section */}
+          <div className="mt-10 flex flex-col items-center lg:items-start gap-4">
+            {/* QR Code */}
+            <Image
+              src="https://i.ibb.co/DXwdPKd/bmc-qr.png"
+              alt="Buy Me a Coffee QR Code"
+              width={150}
+              height={150}
+              className="rounded-md shadow-md"
             />
-          </Link>
-          <Link target="_blank" href="https://t.me/devzahirbot?start=start">
-            <SiTelegram
-              className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-              size={48}
-            />
-          </Link>
-          <Link target="_blank" href="https://www.facebook.com/share/1gjjhoY2Vn/?mibextid=wwXIfr">
-            <FaFacebook
-              className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-              size={48}
-            />
-          </Link>
-          <Link target="_blank" href="https://x.com/devzahirjs">
-            <FaXTwitter
-              className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-              size={48}
-            />
-          </Link>
+
+            {/* Button */}
+            <a
+              href="https://www.buymeacoffee.com/DevZahir"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=DevZahir&button_colour=1a1443&font_colour=ffffff&font_family=Cookie&outline_colour=ffffff&coffee_colour=FFDD00"
+                alt="Buy Me a Coffee"
+                className="w-auto h-12 hover:scale-105 transition-all duration-300"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
