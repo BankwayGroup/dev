9df2e8c8ca54a64e40bbf47d8e0b2e8e179dcae8
BWMarket LLC <zahir@bankway.group>
2025-08-03 00:21:21 -0500

    Update index.jsx

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -43,7 +43,7 @@ function ContactSection() {
 
       {/* GitHub Style Skill Bars */}
       <div className="mb-20 bg-[#0e0c2b] p-6 rounded-xl shadow-xl max-w-4xl mx-auto">
-<h2 className="text-3xl text-[#16f2b3] font-bold text-center mb-8">Tech Stack Overview</h2>
+<h2 className="text-xl text-[#16f2b3] font-bold text-center mb-6">Tech Stack Overview</h2>
 
         <div className="flex flex-col gap-6">
           {skills.map(({ name, percentage }, i) => (
