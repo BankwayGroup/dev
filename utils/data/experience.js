94c343baf48b88112e09f715753fbf5c4e165d85
BankwayLLC <zahir@bankway.group>
2025-08-07 13:12:03 -0500

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -11,8 +11,16 @@ export const experiences = [
     id: 2,
     title: "DevOps & Frontend Developer",
     company: "Personal & Client Projects",
-    duration: "(2020 - Present)",
+    duration: "(2020 - 2024)",
     description:
       "Managed VPS & hosting, used Docker, ngrok; created responsive UIs with React, Next.js, and developed e-commerce platforms.",
+  },
+  {
+    id: 3,
+    title: "AI Automation & SaaS Developer",
+    company: "Indie DevZahir",
+    duration: "(2024 - Present)",
+    description:
+      "Developing AI-integrated tools, SaaS platforms, and intelligent bots with automation pipelines, user dashboards, and Stripe billing.",
   },
 ];
